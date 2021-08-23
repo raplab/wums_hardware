9b20b92a8e9f591d693b7844615a2bc1c250611d
Raplab <raplab@arch.ethz.ch>
2021-08-23 13:11:28 +0200

    Add files via upload

---
+++ loginpanel/RFID_Reader_Script/rfid_to_hid.py
@@ -0,0 +1,43 @@
+#SERIAL TO HID INTERFACE
+#CODE BY: TAO, RAPLAB V1.0, 12.09.2019
+#------------------------------------------------
+#This should run at start-up at the logincomputer
+#------------------------------------------------
+# pip install pyserial
+# pip install pynput
+
+
+import serial
+import time
+from pynput.keyboard import Controller
+
+keyboard = Controller() #configure the keyboard
+
+ser = serial.Serial('/dev/serial0') #init serial in linux '/dev/tty...'
+
+print("RFID reader Init...")
+
+time.sleep(0.5) #add timeout to ensure a propper handshake and avoid stalling
+ser.write(b'pon')
+time.sleep(0.5) #add timeout to ensure a propper handshake and avoid stalling
+ser.write(b'c')
+ser.flushInput() 
+time.sleep(0.5) #timeout to avoid stalling of the port
+
+print("RFID reader configured and ready!")
+
+while True: #running the loop
+    sermessage = ser.readline()
+    if len(sermessage)==16:
+	ser.write(b'.') #abort continous read
+	time.sleep(0.1)
+	ser.write(b's') #read single card
+	smsg = ser.readline()
+        messagestring = smsg.decode("utf-8")
+        cleanmessage = messagestring.rstrip()
+        cleanmessage += "\r" #this enables auto enter
+	print(cleanmessage)
+        keyboard.type(cleanmessage) #use the hid-keyboard to virtually type the result
+        time.sleep(10) #wait in seconds - the user is turing on a machine or doing something else
+	ser.flushInput()
+	ser.write(b'c') #continue with continous mode!
